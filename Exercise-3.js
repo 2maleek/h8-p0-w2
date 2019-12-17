d66da4d01e9cc237be80780df22269104ceda811
Malik Ibrahim Nur <56329434+2maleek@users.noreply.github.com>
2019-12-18 05:27:14 +0700

    ubah '==' jadi '==='

--- Exercise-3.js
+++ Exercise-3.js
@@ -5,11 +5,11 @@ let peran = 'Penyihir';
 
 if(nama && peran) {
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
-    if(peran == 'Ksatria') {
+    if(peran === 'Ksatria') {
         console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`);
-    }else if(peran == 'Tabib') {
+    }else if(peran === 'Tabib') {
         console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`);
-    }else if(peran == 'Penyihir') {
+    }else if(peran === 'Penyihir') {
         console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
     }
 } else if(nama && !peran) {
